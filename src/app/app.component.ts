a1ca6f67524c6c3a9026b1c72dc5163b196db177
Nuru Haque Mazumder <nurnafis95@gmail.com>
2022-11-23 00:43:43 +0600

    bug fix related to sidenav show or hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,7 @@ export class AppComponent implements OnInit {
        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in`);
 
   isSideNavOpen = false;
-
   sideNavMode:string = 'side';
-  isLargeScreen:boolean = true;
 
   constructor(private responsive: BreakpointObserver) {}
 
@@ -43,20 +41,19 @@ export class AppComponent implements OnInit {
         const breakpoints = result.breakpoints
 
         if (breakpoints[Breakpoints.TabletPortrait] || breakpoints[Breakpoints.HandsetPortrait] || breakpoints[Breakpoints.HandsetLandscape]) {
+
           this.sideNavMode = 'over';
-          this.isLargeScreen = false;
-          this.isSideNavOpen = !(this.sideNavMode == 'over' && !this.isLargeScreen);
+
         }
         else if (breakpoints[Breakpoints.TabletLandscape]
           || breakpoints[Breakpoints.Medium]
           || breakpoints[Breakpoints.Large]
           || breakpoints[Breakpoints.XLarge]) {
+
           this.sideNavMode = 'side';
-          this.isLargeScreen = true;
-          this.isSideNavOpen = this.sideNavMode == 'side' && this.isLargeScreen;
         }
 
-        console.log(this.isLargeScreen)
+        console.log(this.sideNavMode)
       })
   }
 
